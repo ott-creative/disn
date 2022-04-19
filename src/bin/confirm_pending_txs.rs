a95b479235b84d09f0b96df1955d5b97ecf07ab7
liyi <15110250679@163.com>
2022-04-19 16:59:26 +0800

    cargo fmt

--- src/bin/confirm_pending_txs.rs
+++ src/bin/confirm_pending_txs.rs
@@ -3,12 +3,11 @@ use disn::CHAIN;
 
 #[tokio::main]
 async fn main() {
-  let mut records = TxRecord::find_by_send_status(0, CHAIN.pool.clone())
-      .await
-      .unwrap()
-      .into_iter();
-  while let Some(record) = records.next() {
-      CHAIN.confirm_tx(record.tx_hash).await;
-  }
-
+    let mut records = TxRecord::find_by_send_status(0, CHAIN.pool.clone())
+        .await
+        .unwrap()
+        .into_iter();
+    while let Some(record) = records.next() {
+        CHAIN.confirm_tx(record.tx_hash).await;
+    }
 }
